9b6d03ccd37cd7ff06bd6024ab4df7e699f56727
guyjof <guyjofre02@gmail.com>
2024-10-10 23:50:22 +0300

    Add script for setting up macOS development environment

---
+++ README.md
@@ -0,0 +1,107 @@
+# Mac Dev Setup
+[![made-for-macOS](https://img.shields.io/badge/Made%20for-macOS-1f425f.svg?logo=apple)](https://www.apple.com/macos/)
+![made-with-bash](https://img.shields.io/badge/Made%20with-Bash-1f425f.svg?logo=gnu-bash)
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+This script allow you to install all the tools you need for setting up your dev environnement on your brand new mac.
+
+# Launch installation
+```shell
+curl https://raw.githubusercontent.com/guyjof/yourrepo/master/setup.sh | bash
+```
+  
+## What does it install?
+
+This script automates the setup of a macOS development environment. It installs various tools, applications, and configurations to get your system ready for development.
+
+### [Xcode Command Line Tools](https://developer.apple.com/xcode/)
+Installs Xcode Command Line Tools required for development.
+
+### [Homebrew](https://brew.sh/)
+The Missing Package Manager for macOS, Homebrew installs the stuff you need that Apple didn't.
+
+### [iTerm2](https://www.iterm2.com/)
+iTerm2 is a replacement for Terminal.
+
+### [ZSH](https://www.zsh.org/) / [oh-my-zsh](https://ohmyz.sh/)
+A delightful community-driven framework for managing your zsh configuration. Includes 200+ optional plugins, over 140 themes, and an auto-update tool.
+
+### CLI Tools
+- **[bat](https://github.com/sharkdp/bat)**: A replacement for cat with syntax highlighting.
+- **[ctop](https://github.com/bcicen/ctop)**: An htop-like interface for container metrics.
+- **[htop](https://github.com/hishamhm/htop)**: An interactive process viewer.
+- **[jq](https://stedolan.github.io/jq/)**: A lightweight and flexible command-line JSON processor.
+- **[lsd](https://github.com/Peltoche/lsd)**: The next gen ls command.
+- **[micro](https://micro-editor.github.io/)**: A modern and intuitive terminal-based text editor for people who don't want to learn vim or emacs.
+- **[tldr](https://github.com/tldr-pages/tldr)**: Simplified and community-driven man pages.
+
+### Fonts
+- **[Nerd Fonts](https://github.com/ryanoasis/nerd-fonts)**: A collection of patched fonts for developers.
+
+### Browser
+- **[Google Chrome](https://www.google.com/chrome/)**: A fast, secure, and free web browser.
+
+### Music / Video
+  - **[Spotify](http://spotify.com/)**: Music streaming.
+
+  - **[VLC](https://www.videolan.org)**: Free and open source cross-platform multimedia player that plays most multimedia files, and various streaming protocols.
+
+### Productivity
+  Development is not coding all day, you also have to collaborate with people and to use you mac for different kind of things.
+- **[Alt-Tab](https://alt-tab-macos.netlify.app/)**: A cmd-tab replacement.
+- **[Notion](https://www.notion.so/)**: A note-taking and collaboration application.
+- **[Rectangle](https://rectangleapp.com/)**: A window manager.
+- **[Raycast](https://raycast.com/)**: An app for developers to control your tools with a few keystrokes.
+- **[Stats](https://github.com/exelban/stats)**: A macOS system monitor in your menu bar.
+- **[Tomatobar](https://github.com/exelban/tomatobar)**: A pomodoro timer in your menu bar.
+
+### Communication
+- **[Slack](https://slack.com/)**: A messaging app for teams.
+
+### Dev Tools
+- **[ngrok](https://ngrok.com/)**: A tunnel localhost over the internet.
+- **[Postman](https://www.postman.com/)**: A tool for sending API requests.
+
+### IDE
+- **[Visual Studio Code](https://code.visualstudio.com/)**: A code editor redefined and optimized for building and debugging modern web and cloud applications.
+
+### Databases
+- **[TimescaleDB](https://www.timescale.com/)**: A time-series database.
+- **[PostgreSQL](https://www.postgresql.org/)**: A powerful, open-source object-relational database system.
+- **[pgcli](https://www.pgcli.com/)**: A command-line interface for PostgreSQL.
+- **[Beekeeper Studio](https://www.beekeeperstudio.io/)**: A database management tool.
+
+### Networking
+- **[Wireshark](https://www.wireshark.org/)**: A network protocol analyzer.
+- **[Netcat](https://en.wikipedia.org/wiki/Netcat)**: A networking utility.
+- **[OpenSSL](https://www.openssl.org/)**: An SSL/TLS cryptography library.
+- **[Telnet](https://en.wikipedia.org/wiki/Telnet)**: A telnet client.
+
+### Docker
+- **[Docker](https://www.docker.com/)**: A containerization platform.
+- **[Docker Compose](https://docs.docker.com/compose/)**: A tool for defining and running multi-container Docker applications.
+- **[Bash Completion](https://docs.docker.com/compose/completion/)**: Bash completion for Docker and Docker Compose.
+
+### Languages
+- **[Node.js](https://nodejs.org/)**: JavaScript runtime built on Chrome's V8 JavaScript engine.
+- **[Go](https://golang.org/)**: An open-source programming language that makes it easy to build simple, reliable, and efficient software.
+- **[Python](https://www.python.org/)**: A programming language that lets you work quickly and integrate systems more effectively.
+- **[Java](https://www.java.com/)**: A high-level, class-based, object-oriented programming language.
+
+### AWS Command Line
+- [**AWS CLI**](https://aws.amazon.com/fr/cli/): Official AWS command line
+
+- [**SAWS**](https://github.com/donnemartin/saws): A supercharged AWS command line interface.
+
+### Python Packages
+- **[Scapy](https://scapy.net/)**: A Python program that enables the user to send, sniff, and dissect and forge network packets.
+
+## Reload Profile Files
+The script will also reload your profile files to apply the changes.
+
+```shell
+source ~/.zshrc
+```
+
+```
+Feel free to customize the content further to match your specific requirements.
---
+++ setup.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+# Create a folder who contains downloaded things for the setup
+INSTALL_FOLDER=~/.macsetup
+mkdir -p $INSTALL_FOLDER
+MAC_SETUP_PROFILE=$INSTALL_FOLDER/macsetup_profile
+
+# Xcode Command Line Tools
+echo "Installing Xcode Command Line Tools"
+softwareupdate -i "Command Line Tools for Xcode" --agree-to-license
+
+# Install Homebrew
+if ! hash brew
+  echo "Installing Homebrew"
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Adding Homebrew to environment"
+  echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  echo "Homebrew is already installed"
+fi
+
+brew install curl wget openssl@3
+
+{
+  echo 'export PATH="/usr/local/opt/curl/bin:$PATH"'
+  echo 'export PATH="/usr/local/opt/openssl@3/bin:$PATH"'
+}>>$MAC_SETUP_PROFILE
+
+# git
+echo "Installing git"
+brew install git
+
+# Install ZSH
+if ! brew list zsh &>/dev/null; then
+  echo "Installing zsh"
+  brew install zsh zsh-completions
+fi
+
+sudo chmod -R 755 /usr/local/share/zsh
+sudo chown -R root:staff /usr/local/share/zsh
+{
+  echo "if type brew &>/dev/null; then"
+  echo "  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH"
+  echo "  autoload -Uz compinit"
+  echo "  compinit"
+  echo "fi"
+} >>$MAC_SETUP_PROFILE
+
+# Set ZSH as the default shell
+if [ "$SHELL" != "/opt/homebrew/bin/zsh" ]; then
+  echo "Setting ZSH as the default shell"
+  chsh -s $(which zsh)
+fi
+
+echo "Pimping out your terminal"
+# Install Oh My Zsh
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install iTerm2
+echo "Installing iTerm2"
+brew install --cask iterm2                          # terminal replacement for MacOS
+
+echo "Installing Powerlevel10k theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Visit: https://github.com/romkatv/powerlevel10k for more information on configuring Powerlevel10k"
+
+# Install cli tools
+echo "Installing CLI tools"
+brew install ack                                    # search tool like grep, but optimized for programmers
+brew install bat                                    # replacement for cat
+brew install ctop                                   # htop-like interface for container metrics
+brew install htop                                   # interactive process viewer
+brew install jq                                     # command-line JSON processor
+brew install lsd                                    # replacement for ls
+brew install micro                                  # replacement for nano/vi
+brew install tldr                                   # Simplified man pages.
+
+# add aliases to profile
+{
+  echo "alias ls='lsd'"
+  echo "alias l='ls -l'"
+  echo "alias la='ls -a'"
+  echo "alias lla='ls -la'"
+  echo "alias lt='ls --tree'"
+} >>$MAC_SETUP_PROFILE
+
+brew install z
+touch ~/.z
+echo '. /usr/local/etc/profile.d/z.sh' >> $MAC_SETUP_PROFILE
+
+# Fonts (https://github.com/ryanoasis/nerd-fonts)
+echo "Installing Nerd Fonts"
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+
+# Browser
+echo "Installing Google Chrome"
+brew install --cask google-chrome
+
+# Music / Video
+echo "Installing Music / Video apps"
+brew install --cask spotify
+brew install --cask vlc
+
+# Productivity
+echo "Installing Productivity apps"
+brew install --cask alt-tab                         # cmd-tab replacement
+brew install --cask notion                          # note taking app
+brew install --cask rectangle                       # window manager
+brew install --cask raycast                         # productivity tool
+brew install --cask stats                           # system monitoring tool
+brew install --cask tomatobar                       # productivity tool
+
+# Communication
+echo "Installing Communication apps"
+brew install --cask slack
+
+# Dev tools
+echo "Installing Dev tools"
+brew install --cask ngrok                           # tunnel localhost over internet.
+brew install --cask postman                         # Postman makes sending API requests simple.
+
+# IDE
+echo "Installing IDEs"
+brew install --cask visual-studio-code              # code editor
+
+# Databases
+echo "Installing Databases and tools"
+brew install timescaledb/tap/timescaledb            # time-series database
+brew install postgresql                             # postgresql database
+brew install pgcli                                  # postgresql client
+brew install --cask beekeeper-studio                # database management tool
+
+# Networking
+echo "Installing Networking tools"
+brew install wireshark                              # network protocol analyzer
+brew install netcat                                 # networking utility
+brew install openssl                                # SSL/TLS cryptography library
+brew install telnet                                 # telnet client
+
+# Docker
+echo "Installing Docker"
+brew install --cask docker                          # containerization platform
+brew install bash-completion                        # bash completion for docker
+brew install docker-compose                         # docker orchestration tool
+brew install docker-completion                      # bash completion for docker
+brew install docker-compose-completion              # bash completion for docker-compose
+
+# Languages
+## Node / JavaScript
+if [ ! -d "$HOME/.nvm" ]; then
+  echo "Installing NVM"
+  brew install nvm
+  echo "export NVM_DIR=\"$HOME/.nvm\"" >> ~/.zprofile
+  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> ~/.zprofile
+  export NVM_DIR="$HOME/.nvm"
+  . "$NVM_DIR/nvm.sh"
+else
+  echo "NVM is already installed"
+fi
+
+echo "Installing Node"
+nvm install node
+echo "Installing pnpm"
+brew install pnpm
+
+## Go
+{
+  echo "# Go development"
+  echo "export GOPATH=\"\${HOME}/.go\""
+  echo "export GOROOT=\"\$(brew --prefix golang)/libexec\""
+  echo "export PATH=\"\$PATH:\${GOPATH}/bin:\${GOROOT}/bin\""
+}>>$MAC_SETUP_PROFILE
+echo "Installing Go"
+brew install go
+
+## python
+echo "export PATH=\"/usr/local/opt/python/libexec/bin:\$PATH\"" >> $MAC_SETUP_PROFILE
+echo "Installing Python"
+brew install python
+pip install --user pipenv
+pip install --upgrade setuptools
+pip install --upgrade pip
+brew install pyenv
+echo 'eval "$(pyenv init -)"' >> $MAC_SETUP_PROFILE
+
+## Java
+echo "Installing Java"
+curl -s "https://get.sdkman.io" | bash                                                               # sdkman is a tool to manage multiple version of java
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java
+brew install maven
+brew install gradle
+
+# Install Python packages
+echo "Installing scapy"
+pip3 install scapy
+
+# AWS command line
+echo "Installing AWS CLI"
+brew install awscli                                 # Official command line
+pip3 install saws                                   # Supercharged AWS CLI with auto-completion
+
+# reload profile files.
+{
+  echo "source $MAC_SETUP_PROFILE # alias and things added by mac_setup script"
+}>>"$HOME/.zshrc"
+
+source "$HOME/.zshrc"
